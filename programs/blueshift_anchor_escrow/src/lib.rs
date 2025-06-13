415d6a03ed3ec1d0b0f60dbecd6433c746c7a65d
Rahul_Bhati <rb083380@gmail.com>
2025-06-13 23:34:41 +0530

    just small bug of speeling mistake

--- programs/blueshift_anchor_escrow/src/lib.rs
+++ programs/blueshift_anchor_escrow/src/lib.rs
@@ -1,20 +1,20 @@
 #![allow(unexpected_cfgs)]
 use anchor_lang::prelude::*;
-
+ 
 mod state;
 mod errors;
 mod instructions;
 use instructions::*;
-
+ 
 declare_id!("22222222222222222222222222222222222222222222");
-
+ 
 #[program]
 pub mod blueshift_anchor_escrow {
     use super::*;
-
+ 
     #[instruction(discriminator = 0)]
-    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, amount: u64) -> Result<()> {
-        make::handler(ctx, seed, receive, amount)
+    pub fn make(ctx: Context<Make>, seed: u64, recieve: u64, amount: u64) -> Result<()> {
+        make::handler(ctx, seed, recieve, amount)
     }
  
     #[instruction(discriminator = 1)]
@@ -23,7 +23,7 @@ pub mod blueshift_anchor_escrow {
     }
  
     #[instruction(discriminator = 2)]
-    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+     pub fn refund(ctx: Context<Refund>) -> Result<()> {
         refund::handler(ctx)
     }
 }
